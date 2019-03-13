5e2e8a5cf23a24e427a00cb9ec1af913818f5efc
pardoalejo <la.pardo2014@uniandes.edu.co>
2019-03-12 20:06:55 -0500

    args and prints added

--- maskrcnn_benchmark/utils/pred2ann.py
+++ maskrcnn_benchmark/utils/pred2ann.py
@@ -3,6 +3,7 @@ from pycocotools.cocoeval import COCOeval
 import os
 import os.path as osp
 import json
+import argparse
 
 def pred2hard(ann_file, pred_file, output, fp_th=0.8):
     
@@ -20,25 +21,64 @@ def pred2hard(ann_file, pred_file, output, fp_th=0.8):
     new_anns = json.load(open(ann_file))
 
     #Find the FPs given a threshold
+    print('Finding FP...')
+    num_fp = 0
     for key in cocoDt.imgToAnns.keys():
         #ipdb.set_trace()
         ious = cocoEval.ious[(key,1)]
         for i,iou in enumerate(ious):
             max_overlap = max(iou)
             if max_overlap <= fp_th:
+                num_fp += 1
                 fp = cocoDt.imgToAnns[key][i]
                 fp['category_id'] = 2
                 fp['id'] += num_train_inst
                 fp['width'] = fp['segmentation']['size'][1]
                 fp['height'] = fp['segmentation']['size'][0]
                 new_anns['annotations'].append(fp)
-
+    print('{} FPs found'.format(num_fp))
+    
     new_anns['categories'].append({'id':2, 'name': 'Three Leaf Clover', 'supercategory': 'Clover'})
     with open(output) as f:
         json.dump(new_anns, f)
+    
+    print('Hard negatives annotation file saved in {}'.format(output))
 if __name__ == "__main__":
-    ann_file = 'examples/instances_test_pos.json'
-    pred_file = 'examples/segm.json'
-    output = 'examples/instances_test_hard_negatives.json'
-    pred2hard(ann_file, pred_file, output)
+
+    parser = argparse.ArgumentParser(description="Predictions to hard negatives")
+    parser.add_argument(
+        "--ann_file",
+        default="",
+        help="path to ann file",
+        required=True,
+        type=str,
+    )
+    parser.add_argument(
+        "--pred_file",
+        default="",
+        help="path to pred file",
+        required=True,
+        type=str,
+    )
+    parser.add_argument(
+        "--out_file",
+        default="",
+        help="name and path of out file",
+        required=True,
+        type=str,
+    )
+    parser.add_argument(
+        "--th",
+        default=0.8,
+        help="FP threshold",
+        required=False,
+        type=int,
+    )
+
+    args = parser.parse_args()
+    print('Function called with args {}'.format(args))
+    #ann_file = 'examples/instances_test_pos.json'
+    #pred_file = 'examples/segm.json'
+    #output = 'examples/instances_test_hard_negatives.json'
+    pred2hard(args.ann_file, args.pred_file, args.output)
     
