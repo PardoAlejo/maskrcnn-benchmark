eef8fddbf8ba052522421e1399c57a39ad39a273
pardoalejo <la.pardo2014@uniandes.edu.co>
2019-03-13 19:14:47 -0500

    Probability of gliping added to configs

--- maskrcnn_benchmark/config/defaults.py
+++ maskrcnn_benchmark/config/defaults.py
@@ -53,7 +53,8 @@ _C.INPUT.PIXEL_MEAN = [102.9801, 115.9465, 122.7717]
 _C.INPUT.PIXEL_STD = [1., 1., 1.]
 # Convert image to BGR format (for Caffe2 models), in range 0-255
 _C.INPUT.TO_BGR255 = True
-
+# Probability of flipping an image during training
+_C.INPUT.FLIP_PROB_TRAIN = 0.5
 
 # -----------------------------------------------------------------------------
 # Dataset
