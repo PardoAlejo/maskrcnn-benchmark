1ab1e06a793bc771570d034261c247fd5767d35b
Luis Alejandro Pardo <lapardo@advicetec.com>
2019-03-19 11:17:07 -0500

    Right paths

--- maskrcnn_benchmark/config/paths_catalog.py
+++ maskrcnn_benchmark/config/paths_catalog.py
@@ -133,8 +133,8 @@ class DatasetCatalog(object):
             "ann_file": "flc/trainval/coco_annotations/instances_trainval_hard_pos.json"
         },
         "flc_test_hards_instances_cocostyle": {
-            "img_dir": "flc/trainval/JPEGImages",
-            "ann_file": "flc/trainval/coco_annotations/instances_test_hard_pos.json"
+            "img_dir": "flc/test/JPEGImages",
+            "ann_file": "flc/test/coco_annotations/instances_test_hard_pos.json"
         },
         "flc_trainval_hards_leaves_cocostyle": {
             "img_dir": "flc/trainval/JPEGImages",
