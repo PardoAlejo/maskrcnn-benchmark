51a67145b378896e3512df35b132b59f0e46ce72
Luis Alejandro Pardo <lapardo@advicetec.com>
2019-03-19 10:37:20 -0500

    Added test for hards

--- maskrcnn_benchmark/config/paths_catalog.py
+++ maskrcnn_benchmark/config/paths_catalog.py
@@ -132,6 +132,10 @@ class DatasetCatalog(object):
             "img_dir": "flc/trainval/JPEGImages",
             "ann_file": "flc/trainval/coco_annotations/instances_trainval_hard_pos.json"
         },
+        "flc_test_hards_instances_cocostyle": {
+            "img_dir": "flc/trainval/JPEGImages",
+            "ann_file": "flc/trainval/coco_annotations/instances_test_hard_pos.json"
+        },
         "flc_trainval_hards_leaves_cocostyle": {
             "img_dir": "flc/trainval/JPEGImages",
             "ann_file": "flc/trainval/coco_annotations/leaves_trainval_hard_pos.json"
