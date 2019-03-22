55a26895870f415f27f7cd06c9ae52b23c10b8b5
Luis Alejandro Pardo <lapardo@advicetec.com>
2019-03-21 21:39:28 -0500

    Train hard negs

--- maskrcnn_benchmark/config/paths_catalog.py
+++ maskrcnn_benchmark/config/paths_catalog.py
@@ -132,6 +132,10 @@ class DatasetCatalog(object):
             "img_dir": "flc/trainval/JPEGImages",
             "ann_file": "flc/trainval/coco_annotations/instances_trainval_hard_pos.json"
         },
+        "flc_trainval_hards_complete_instances_cocostyle": {
+            "img_dir": "flc/trainval/JPEGImages",
+            "ann_file": "flc/trainval/coco_annotations/instances_trainval_complete.json"
+        }
         "flc_test_hards_instances_cocostyle": {
             "img_dir": "flc/test/JPEGImages",
             "ann_file": "flc/test/coco_annotations/instances_test_hard_pos.json"
